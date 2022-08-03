fc1edc94583ead4251bae113d329ec93af7373b2
Zlata27 <zgaivoronskaia@gmail.com>
2022-08-03 23:11:58 +0300

    Add new files

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 10000; // Стоимость билета
+        int miles = 20; // Чему равна одна бонусная миля (20 руб)
+
+        System.out.println((amount / miles) + " бонусных миль");
+
+    }
+}
